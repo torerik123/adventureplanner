9fe4ad200418b849eeb7ee0ada4075787b8c3fee
torerik123 <tor_erik_grimen@hotmail.com>
2020-11-14 01:39:30 +0100

    prepare for heroku deploy

--- application.py
+++ application.py
@@ -32,8 +32,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///adventure.db")
+# Configure CS50 Library to use Postgres database
+db = SQL("postgres://wocjtrmwcfrwai:fb2d70589ffd96cd9bd4473bf9590ca26798da3769586512a49a41c3b1129df1@ec2-54-228-250-82.eu-west-1.compute.amazonaws.com:5432/da5k6k2pjfrag7")
 
 @app.route("/")
 def landingpage():
