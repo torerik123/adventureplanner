27126e0ba55700f2ff471584344624ba8b7805d8
torerik123 <tor_erik_grimen@hotmail.com>
2020-11-18 11:41:46 +0100

    heroku error

--- application.py
+++ application.py
@@ -29,8 +29,8 @@ def after_request(response):
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-#app.config["SESSION_TYPE"] = "filesystem"
-app.config["SESSION_TYPE"] = "redis"  
+app.config["SESSION_TYPE"] = "filesystem"
+# #app.config["SESSION_TYPE"] = "filesystem"  
 Session(app)
 
 # Configure CS50 Library to use Postgres database
